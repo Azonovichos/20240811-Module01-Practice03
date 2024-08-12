4e046e8f6485025d22b6502be3e959187604938a
Marsel <kms-official@yandex.ru>
2024-08-12 16:42:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Module 1 Practice 3
+name = 'Marsel'
+print ('Name:',name)
+age = 38
+print ('Age:',age)
+age = age + 1
+print ('New age:',age)
+is_student = True
+print ('Is Student:',is_student)
